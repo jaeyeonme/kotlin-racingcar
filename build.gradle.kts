6bf1ceae42263947e7237ca073a163e5b6ec7b51
Jaeyeon Cho <cjyeon1022@gmail.com>
2026-09-03 16:02:50 +0900

    build: upgrade to JDK 25

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
-    kotlin("jvm") version "1.8.10"
-    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+    kotlin("jvm") version "2.4.10"
+    id("org.jmailen.kotlinter") version "5.7.0"
 }
 
 group = "camp.nextstep.edu"
@@ -11,22 +13,30 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter", "junit-jupiter", "5.8.2")
-    testImplementation("org.assertj", "assertj-core", "3.22.0")
-    testImplementation("io.kotest", "kotest-runner-junit5", "5.2.3")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation("org.assertj:assertj-core:3.22.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.8.2")
+}
+
+kotlin {
+    jvmToolchain(25)
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_25)
+    }
 }
 
 tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "11"
-    }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "11"
-    }
     test {
         useJUnitPlatform()
     }
-    ktlint {
-        verbose.set(true)
+    register("ktlintCheck") {
+        group = "verification"
+        description = "Runs ktlint on all Kotlin sources."
+        dependsOn("lintKotlin")
     }
+}
+
+kotlinter {
+    ktlintVersion = "1.8.0"
+    reporters = arrayOf("plain")
 }
